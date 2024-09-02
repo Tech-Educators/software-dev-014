6e2337c7970e2bc5dbd7f0fea8d5dccea72974f8
Manuel <manugonzalez.tech@gmail.com>
2024-09-02 10:14:37 +0100

    Add variables demo

---
+++ demos/week2/variables/app.js
@@ -0,0 +1,31 @@
+// I want to log a message in the console
+console.log("Is this workinnnngggg????");
+
+// declaring variables
+// var --> this is deprecated and AI uses it all the time...
+//we use two keywords to declare variables: let and const
+//let variableName = value
+//we use camelcase for JS
+//we can only declare a variable ONCE
+let myName = "Manny";
+console.log(myName);
+
+//const variableName = value
+const mySecondName = "Joe";
+console.log(mySecondName);
+
+//the difference between const and let
+myName = "Tim";
+console.log(myName);
+
+//we cannot assign a new value to a constant variable; it throws an error
+// mySecondName = "Sam";
+// console.log(mySecondName);
+
+//what if I want to log TWO variables in my console???
+console.log(myName);
+console.log(mySecondName);
+
+//I can use the + symbol to concatenate variables
+console.log(myName + mySecondName);
+console.log(myName + " " + mySecondName + "!");

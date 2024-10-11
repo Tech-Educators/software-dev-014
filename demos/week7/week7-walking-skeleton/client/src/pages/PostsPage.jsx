4aa8d8b0d9dc58ce78afc82ba45b714c23cc349a
Manuel <manugonzalez.tech@gmail.com>
2024-10-11 14:27:01 +0100

    Add amend

--- demos/week7/week7-walking-skeleton/client/src/pages/PostsPage.jsx
+++ demos/week7/week7-walking-skeleton/client/src/pages/PostsPage.jsx
@@ -1,5 +1,6 @@
 //Here I will fetch the posts from the server (get endpoint), which is getting the posts from the db
 export default function PostsPage() {
+  //we need useState to save the fetched data
   //we need useEffect to fetch the data
   //useEffect()
   //we need a function to fetchData(){
@@ -7,6 +8,8 @@ export default function PostsPage() {
   //parse the data into json
   //console log your dat, so you can see what it looks like
   //you might have to wrangle data, depending on what your data looks like
+  //set your state variable to be the fetched data
+  //setState(fetchedData)
   //!Once you have finished your app and your deployment is ready, replace your local host urls with the Render urls
   // }
   return (
